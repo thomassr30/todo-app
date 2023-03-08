151398427bea864df8306625777b087bf6f3173c
Thomas Soto <thomassoto94@gmail.com>
2023-03-08 13:45:03 -0300

    create project todo app

---
+++ data/todo.js
@@ -0,0 +1,25 @@
+const todosData = [
+    {
+        id: 1,
+        text: 'Cocinar',
+        isCompleted: true,
+        isToday: true,
+        hour: '13:00'
+    },
+    {
+        id: 2,
+        text: 'Clases industrial',
+        isCompleted: false,
+        isToday: false,
+        hour: '19:00'
+    },
+    {
+        id: 3,
+        text: 'Estudiar',
+        isCompleted: true,
+        isToday: true,
+        hour: '21:00'
+    },
+]
+
+module.exports = {todosData}
